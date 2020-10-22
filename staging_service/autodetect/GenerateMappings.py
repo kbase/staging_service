abcc2ace951ee3f6518e9d8c79780cffd3afa324
bio-boris <bioboris@github.com>
2020-10-22 15:21:47 -0500

    adding some generated configs

--- staging_service/autodetect/GenerateMappings.py
+++ staging_service/autodetect/GenerateMappings.py
@@ -4,23 +4,32 @@
 * Serve that generated content from memory
 
 """
+import json
 from collections import defaultdict
+import yaml
 
+# Regular Formats
+JSON = "JSON"
 TSV = "TSV"
+CSV = "CSV"
+EXCEL = "EXCEL"
+ZIP = "CompressedFileFormatArchive"
+
+# BIOINFORMATICS FORMATS
 FASTA = "FASTA"
 FASTQ = "FASTQ Reads"
 GFF = "GFF"
-CSV = "CSV"
-EXCEL = "EXCEL"
 GTF = "GTF"
 SRA = "SRA"
 SAM = "SAM"
 BAM = "BAM"
 GENBANK = "GENBANK"
 VCF = "VCF"
-MSA = "MultipleSequenceAlignment"
-ZIP = "CompressedFileFormatArchive"
 FBA = "FBAModel"
+SBML = "SBML"
+
+# KBASE SPECIFIC FORMATS
+MSA = "MultipleSequenceAlignment"
 PHENOTYPE = "PHENOTYPE"
 ESCHER = "ESCHER"
 ANNOTATIONS = "ANNOTATIONS"
@@ -55,12 +64,18 @@ type_to_extension_mapping = {
         "phylip",
         "stockholm",
     ],
+    TSV: ["tsv"],
+    CSV: ["csv"],
+    JSON: ["json"],
+    EXCEL: ["xls", "xlsx"],
     ZIP: ["zip", "tar", "tgz", "tar.gz", "7z", "gz", "gzip", "rar"],
+    SBML: ["smbl"],
+    # Custom File Types?
     MEDIA: ["tsv", "xls", "xlsx"],
     PHENOTYPE: ["tsv"],
     ESCHER: ["json"],
     ANNOTATIONS: ["tsv"],
-    FBA: [],
+    FBA: ["tsv", "xls", "xlsx", "smbl"],
 }
 
 mapping = defaultdict(list)
@@ -128,7 +143,7 @@ mapping[MSA] = [
 
 mapping[GENBANK] = [
     {
-        "title": "Genbank Genome ",
+        "title": "Genbank Genome",
         "app": "kb_uploadmethods/import_genbank_as_genome_from_staging",
         "output_type": ["KBaseGenomes.Genome"],
     },
@@ -161,15 +176,93 @@ mapping[ZIP] = [
     {
         "title": "Decompress/Unpack",
         "app": "kb_uploadmethods/unpack_staging_file",
-        "output_type": [],
+        "output_type": [None],
     }
 ]
-mapping[CSV] = \
+mapping[CSV] = [
+    {
+        "title": "Import Samples",
+        "app": "sample_uploader/import_samples",
+        "output_type": ["KBaseSets.SampleSet"],
+    }
+]
 
+mapping[TSV] = [
+    {
+        "title": "Media",
+        "app": "kb_uploadmethods/import_tsv_excel_as_media_from_staging",
+        "output_type": ["KBaseBiochem.Media"],
+    },
+    {
+        "title": "Attribute Mapping",
+        "app": "kb_uploadmethods/import_attribute_mapping_from_staging",
+        "output_type": ["KBaseExperiments.AttributeMapping"],
+    },
+    {
+        "title": "Expression Matrix",
+        "app": "kb_uploadmethods/import_tsv_as_expression_matrix_from_staging",
+        "output_type": ["KBaseFeatureValues.ExpressionMatrix"],
+    },
+    {
+        "title": "Metabolic Annotations",
+        "app": "MergeMetabolicAnnotations/import_annotations",
+        "output_type": ["KBaseGenomes.Genome"],
+    },
+    {
+        "title": "Bulk Metabolic Annotations",
+        "app": "MergeMetabolicAnnotations/import_bulk_annotations",
+        "output_type": ["KBaseGenomes.Genome"],
+    },
+    {
+        "title": "FBA Model",
+        "app": "kb_uploadmethods/import_file_as_fba_model_from_staging",
+        "output_type": ["KBaseFBA.FBAModel"],
+    },
+    {
+        "title": "Phenotype Set",
+        "app": "kb_uploadmethods/import_tsv_as_phenotype_set_from_staging",
+        "output_type": ["KBasePhenotypes.PhenotypeSet"],
+    },
+]
 
-    ["sample_uploader/import_samples"]
-mapping[EXCEL] = ["sample_uploader/import_samples"]
-mapping[TSV] = ["kb_uploadmethods/import_tsv_as_expression_matrix_from_staging"]
+mapping[EXCEL] = [
+    {
+        "title": "Samples",
+        "app": "sample_uploader/import_samples",
+        "output_type": ["KBaseSets.SampleSet"],
+    },
+    {
+        "title": "Media",
+        "app": "kb_uploadmethods/import_tsv_excel_as_media_from_staging",
+        "output_type": ["KBaseBiochem.Media"],
+    },
+    {
+        "title": "Attribute Mapping",
+        "app": "kb_uploadmethods/import_attribute_mapping_from_staging",
+        "output_type": ["KBaseExperiments.AttributeMapping"],
+    },
+    {
+        "title": "FBA Model",
+        "app": "kb_uploadmethods/import_file_as_fba_model_from_staging",
+        "output_type": ["KBaseFBA.FBAModel"],
+    },
+]
+mapping[JSON] = [
+    {
+        "title": "EscherMap",
+        "app": "kb_uploadmethods/import_eschermap_from_staging",
+        "output_type": ["KBaseFBA.EscherMap"],
+    }
+]
+
+mapping[SBML] = [
+    {
+        "title": "FBA Model",
+        "app": "kb_uploadmethods/import_file_as_fba_model_from_staging",
+        "output_type": ["KBaseFBA.FBAModel"],
+    }
+]
+
 
 extension_to_type_mapping = defaultdict(list)
 extension_to_app_mapping = defaultdict(list)
@@ -177,16 +270,17 @@ for datatype in type_to_extension_mapping:
     for file_extension in type_to_extension_mapping[datatype]:
         extension_to_type_mapping[file_extension.lower()].append(datatype)
         app = mapping[datatype]
-        extension_to_app_mapping[file_extension.lower()].append(app)
+        extension_to_app_mapping[file_extension.lower()].extend(app)
 
 extension_to_type_mapping = extension_to_type_mapping
 extension_to_app_mapping = extension_to_app_mapping
 
-import json
-
 print("Extension to type mapping")
-print(json.dumps(extension_to_type_mapping, indent=2))
+with open("./autodetect/extension_to_type.json", "w") as f:
+    print(json.dumps(extension_to_type_mapping, indent=2))
+    json.dump(obj=extension_to_type_mapping, fp=f, indent=2)
 
 print("Extension to app mapping")
-
-print(json.dumps(extension_to_app_mapping, indent=2))
+with open("./autodetect/extension_to_app.json", "w") as f:
+    print(json.dumps(extension_to_app_mapping, indent=2))
+    json.dump(obj=extension_to_app_mapping, fp=f, indent=2)
