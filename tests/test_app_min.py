958aba7f64485697ecf3940ecb6fc9516e65974f
Gavin <gaprice@lbl.gov>
2021-07-09 16:35:05 -0700

    remove fileutil - more simplification

--- tests/test_app_min.py
+++ tests/test_app_min.py
@@ -22,46 +22,20 @@ class AppClient:
         await self.client.close()
 
 
-class FileUtil:
-    def __init__(self, base_dir=DATA_DIR):
-        self.base_dir = base_dir
-
-    def __enter__(self):
-        os.makedirs(self.base_dir, exist_ok=True)
-        shutil.rmtree(self.base_dir)
-        os.makedirs(self.base_dir, exist_ok=False)
-        return self
-
-    def __exit__(self, *args):
-        shutil.rmtree(self.base_dir)
-
-    def make_file(self, path, contents):
-        path = os.path.join(self.base_dir, path)
-        with open(path, encoding="utf-8", mode="w") as f:
-            f.write(contents)
-        return path
-
-    def make_dir(self, path):
-        path = os.path.join(self.base_dir, path)
-        os.makedirs(path, exist_ok=True)
-        return path
-
-    def remove_dir(self, path):
-        shutil.rmtree(path)
-
-
 async def test_upload():
+    testdir = os.path.normpath(os.path.join(os.getcwd(), './temptest/'))
+    os.makedirs(testdir, exist_ok=True)
     async with AppClient() as cli:
+        await do_thing(cli, testdir, "test_file_1")
+        await do_thing(cli, testdir, "test_file_2")
 
-        with FileUtil() as fs:
-            d = fs.make_dir(os.path.join("test"))
-            await do_thing(fs, cli, "test_file_1")
-            await do_thing(fs, cli, "test_file_2")
 
-async def do_thing(fs, cli, filename):
-    f = fs.make_file(os.path.join("test", filename), "testtext")
+async def do_thing(cli, testdir, filename):
+    path = os.path.join(testdir, filename)
+    with open(path, encoding="utf-8", mode="w") as f:
+        f.write("testtext")
 
-    files = {"uploads": open(f, "rb")}
+    files = {"uploads": open(path, "rb")}
 
     res = await cli.post(
         os.path.join("upload"), headers={"Authorization": ""}, data=files
