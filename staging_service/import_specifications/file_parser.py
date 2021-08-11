592ddfb79131326258b0b7688b9452d9b1f6c6fd
Gavin <gaprice@lbl.gov>
2021-08-11 15:32:16 -0700

    Update error structures based on ADR updates

--- staging_service/import_specifications/file_parser.py
+++ staging_service/import_specifications/file_parser.py
@@ -37,6 +37,35 @@ class SpecificationSource:
 
 
 @dataclass(frozen=True)
+class Error:
+    f"""
+    An error found while attempting to parse files.
+
+    error - the type of the error.
+    message - the error message, if any
+    source - the data source associated with the error, if any
+
+    Each error type has different required arguments:
+    {ErrorType.FILE_NOT_FOUND.name}: source
+    {ErrorType.PARSE_FAIL}: source and message
+    {ErrorType.OTHER}: message. source is optional if the error applies to a source file.
+
+    """
+    error: ErrorType
+    message: str = None
+    source: SpecificationSource = None
+
+    def __post_init__(self):
+        if self.error == ErrorType.FILE_NOT_FOUND and not self.source:
+            raise ValueError(f'source is required for a {ErrorType.FILE_NOT_FOUND.name} error')
+        if self.error == ErrorType.PARSE_FAIL and (not self.source or not self.message):
+            pf = ErrorType.PARSE_FAIL.name
+            raise ValueError(f'source and message are required for a {pf} error')
+        if self.error == ErrorType.OTHER and not self.message:
+            raise ValueError(f'message is required for a {ErrorType.OTHER.name} error')
+
+
+@dataclass(frozen=True)
 class ParseResults:
     """
     Contains the results of parsing import specification files.
@@ -44,26 +73,20 @@ class ParseResults:
     results - the parse results. A mapping from the data type, to a tuple containing
         1) the source of the results
         2) the data for the data type as a tuple of dicts.
-    errortype - the type of any errors encountered while parsing the files.
-    errors - error messages on a per data source basis.
+    errors - the errors encountered while parsing the files, if any.
 
-    Either results or both errotype and errors must be provided.
+    Either results or errors must be provided.
     Other than the above check, no other error checking is done by this class, and it is
     expected that the class creator do that error checking. Users should use the
     parse_import_specifications method to create an instance of this class.
     """
     results: frozendict[str, Tuple[SpecificationSource,
         Tuple[frozendict[str, PRIMITIVE_TYPE]]]] = None
-    errortype: ErrorType = None
-    errors: frozendict[SpecificationSource, str] = None
+    errors: Tuple[Error] = None
 
     def __post_init__(self):
-        if self.results and (self.errortype or self.errors):
-            raise ValueError("Cannot include both results and errors")
-        if not self.results and (bool(self.errortype) ^ bool(self.errors)):  # xor
-            raise ValueError("Both or neither of the errortype and error are required")
-        if not self.results and not self.errortype:
-            raise ValueError("At least one of results or errors are required")
+        if not (bool(self.results) ^ bool(self.errors)):  # xnor
+            raise ValueError("Exectly one of results or errors must be supplied")
         # we assume here that the data is otherwise correctly created.
 
 
--- tests/import_specifications/test_file_parser.py
+++ tests/import_specifications/test_file_parser.py
@@ -7,55 +7,104 @@ from tests.test_utils import assert_exception_correct
 from staging_service.import_specifications.file_parser import (
     ErrorType,
     SpecificationSource,
+    Error,
     ParseResults,
 )
+
+
+def test_Error_init_w_FILE_NOT_FOUND_success():
+    # minimal
+    e = Error(ErrorType.FILE_NOT_FOUND, source=SpecificationSource("foo"))
+
+    assert e.error == ErrorType.FILE_NOT_FOUND
+    assert e.message is None
+    assert e.source == SpecificationSource("foo")
+
+    # all
+    e = Error(ErrorType.FILE_NOT_FOUND, message="bar", source=SpecificationSource("foo"))
+
+    assert e.error == ErrorType.FILE_NOT_FOUND
+    assert e.message == "bar"
+    assert e.source == SpecificationSource("foo")
+
+
+def test_Error_init_w_PARSE_FAIL_success():
+    e = Error(ErrorType.PARSE_FAIL, message="foo", source=SpecificationSource("foo2"))
+
+    assert e.error == ErrorType.PARSE_FAIL
+    assert e.message == "foo"
+    assert e.source == SpecificationSource("foo2")
+
+
+def test_Error_init_w_OTHER_success():
+    # minimal
+    e = Error(ErrorType.OTHER, message="foo")
+
+    assert e.error == ErrorType.OTHER
+    assert e.message == "foo"
+    assert e.source == None
+
+    # all
+    e = Error(ErrorType.OTHER, message="foo", source=SpecificationSource("whoo"))
+
+    assert e.error == ErrorType.OTHER
+    assert e.message == "foo"
+    assert e.source == SpecificationSource("whoo")
+
+
+def test_Error_init_fail():
+    error_init_fail(ErrorType.FILE_NOT_FOUND, None, None, ValueError(
+        "source is required for a FILE_NOT_FOUND error"))
+    err = "source and message are required for a PARSE_FAIL error"
+    error_init_fail(ErrorType.PARSE_FAIL, None, SpecificationSource("foo"), ValueError(err))
+    error_init_fail(ErrorType.PARSE_FAIL, "msg", None, ValueError(err))
+    error_init_fail(ErrorType.OTHER, None, None, ValueError(
+        "message is required for a OTHER error"))
+
+
+def error_init_fail(errortype, message, source, expected):
+    with raises(Exception) as got:
+        Error(errortype, message, source)
+    assert_exception_correct(got.value, expected)
+
 
 PR_RESULTS = frozendict({"data_type": (SpecificationSource("some_file", "tab"), (
     frozendict({"fasta_file": "foo.fa", "do_thing": 1}),  # make a tuple!
 ))})
 
-PR_ERROR = frozendict({
-    SpecificationSource("some_file"): "ah crehp",
-    SpecificationSource("other_file", "tab3"): "that sucks bro",
-})
+PR_ERROR = (
+    Error(ErrorType.OTHER, message="foo"),
+    Error(ErrorType.PARSE_FAIL, message="bar", source=SpecificationSource("some_file", "tab3"))
+)
 
 def test_ParseResults_init_w_results_success():
     results_copy = frozendict(PR_RESULTS)  # prevent identity equality
 
     pr = ParseResults(PR_RESULTS)
     assert pr.results == results_copy
-    assert pr.errortype is None
     assert pr.errors is None
 
     assert pr == ParseResults(results_copy)
 
 
 def test_ParseResults_init_w_error_success():
-    errors_copy = frozendict(PR_ERROR) # prevent identity equality
+    errors_copy = tuple(PR_ERROR) # prevent identity equality
 
-    pr = ParseResults(errortype=ErrorType.PARSE_FAIL, errors=PR_ERROR)
+    pr = ParseResults(errors=PR_ERROR)
     assert pr.results is None
-    assert pr.errortype == ErrorType.PARSE_FAIL
     assert pr.errors == errors_copy
 
-    assert pr == ParseResults(errortype=ErrorType.PARSE_FAIL, errors=errors_copy)
+    assert pr == ParseResults(errors=errors_copy)
 
 
 def test_ParseResults_init_fail():
-    parseResults_init_fail(None, None, None, ValueError(
-        "At least one of results or errors are required"))
-    parseResults_init_fail(PR_RESULTS, ErrorType.OTHER, None, ValueError(
-        "Cannot include both results and errors"))
-    parseResults_init_fail(PR_RESULTS, None, PR_ERROR, ValueError(
-        "Cannot include both results and errors"))
-    parseResults_init_fail(None, ErrorType.OTHER, None, ValueError(
-        "Both or neither of the errortype and error are required"))
-    parseResults_init_fail(None, None, PR_ERROR, ValueError(
-        "Both or neither of the errortype and error are required"))
+    err = 'Exectly one of results or errors must be supplied'
+    parseResults_init_fail(None, None, ValueError(err))
+    parseResults_init_fail(PR_RESULTS, PR_ERROR, ValueError(err))
 
 
-def parseResults_init_fail(results, errortype, errors, expected):
+def parseResults_init_fail(results, errors, expected):
     with raises(Exception) as got:
-        ParseResults(results, errortype, errors)
+        ParseResults(results, errors)
     assert_exception_correct(got.value, expected)
 
