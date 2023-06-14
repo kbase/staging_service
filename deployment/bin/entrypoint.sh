5894114ce94baadcd800fe4ba2429f6f85937e3c
Erik Pearson <eapearson@lbl.gov>
2023-06-14 11:33:50 -0700

    refactor entrypoint to use gunicorn and remove local dev support [PTV-1882]
    The production entrypoint should not serve a dual purpose - local dev and prod runtime. The local dev support needs work anyway, as it relies on Visual Studio Code, which is out of date.
    Gunicorn is used now as it helps ameliorate stuck aiohttp servers and relaunching crashed aiothttp servers. It's configuration uses env variables, all with defaults (which may need to thought about more deeply and/or tested.)

--- deployment/bin/entrypoint.sh
+++ deployment/bin/entrypoint.sh
@@ -3,17 +3,40 @@
 # please also update the launch.json for vscode accordingly
 # the things that must be kept in sync are KB_DEPLOYMENT_CONFIG and PYTHONPATH
 
-#top section for local running
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-if [ -d "$DIR/../../staging_service" ]; then
-    PYTHONPATH="$DIR/../../staging_service"
-    export KB_DEPLOYMENT_CONFIG="$DIR/../conf/local.cfg"
-    export FILE_LIFETIME="90"
+
+if [ -d "kb/deployment/lib/staging_service" ]; then
+   echo "staging_service not installed in /kb/deployment/lib as expected"
+   exit 1
 fi
 
-#bottom section for running inside docker
-if [ -d "kb/deployment/lib/staging_service" ]; then
-    PYTHONPATH="kb/deployment/lib/staging_service"
-    # environment variable for KB_DEPLOYMENT_CONFIG set in docker-compose.yml
-fi
-python3 -m staging_service
+PYTHONPATH="kb/deployment/lib/staging_service"
+
+# disabled in favor of running behind gunicorn
+# python3 -m staging_service
+
+# The port at which to run this service.
+# TODO: this should probably be 5000 in order to operate like other KBase services.
+#       Who likes surprises like this?
+SERVICE_PORT="${SERVICE_PORT:-3000}"
+
+# See https://docs.gunicorn.org/en/latest/run.html
+# Recommended to have 2-4 workers per core. Default assumes 1 core.
+GUNICORN_WORKERS="${GUNICORN_WORKERS:-4}"
+
+# Workers silent for more than this many seconds are killed and restarted.
+# This setting affects the /upload endpoint, as it processes files after
+# upload, calculating the md5 and potentially, in the future, additional tasks
+# such as validating the file. 
+# Other endpoints may encounter a delay as well, as any endpoint which access
+# metadata may trigger a metadata rebuild, including the md5.
+# Default to 10 minutes, a generous amount of time to wait for this to complete.
+# Alternative designs could solve this in a different way.
+GUNICORN_TIMEOUT="${GUNICORN_TIMEOUT:-600}"
+
+# TODO: should be port 5000 to match other kbase services
+gunicorn staging_service.main:web_app \
+  --bind 0.0.0.0:"${SERVICE_PORT}" \
+  --worker-class aiohttp.GunicornWebWorker \
+  --pythonpath "${PYTHONPATH}" \
+  --workers "${GUNICORN_WORKERS}" \
+  --timeout "${GUNICORN_TIMEOUT}"
