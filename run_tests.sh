153f85250152d2503120e858a4fb905166b19470
Erik Pearson <eapearson@lbl.gov>
2023-09-01 17:54:48 -0700

    don't run as regular user. [PTV-1888]

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-export KB_DEPLOYMENT_CONFIG="$DIR/deployment/conf/testing.cfg"
+export KB_DEPLOYMENT_CONFIG="${DIR}/deployment/conf/testing.cfg"
 export FILE_LIFETIME="90"
 export TESTS="${1:-tests}"
 echo
