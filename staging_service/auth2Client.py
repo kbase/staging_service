eb7ca87239dd3ed80bd801359b97a0e98feace27
Bill Riehl <wjriehl@lbl.gov>
2025-06-26 12:16:48 -0400

    remove app.py changes from last commit

--- staging_service/auth2Client.py
+++ staging_service/auth2Client.py
@@ -10,18 +10,21 @@ import time as _time
 
 import aiohttp
 
+TOKEN_PATH = "/api/V2/token"
+USERNAME_PATH = "/login/suggestname/"
 
-class TokenCache(object):
+
+class TokenCache:
     """A basic cache for tokens."""
 
     _MAX_TIME_SEC = 5 * 60  # 5 min
 
-    def __init__(self, maxsize=2000):
+    def __init__(self, maxsize: int = 2000):
         self._cache = {}
         self._maxsize = maxsize
         self._halfmax = maxsize / 2  # int division to round down
 
-    def get_user(self, token):
+    def get_user(self, token: str):
         token = hashlib.sha256(token.encode("utf8")).hexdigest()
         usertime = self._cache.get(token)
         if not usertime:
@@ -33,7 +36,7 @@ class TokenCache(object):
             return None
         return user
 
-    def add_valid_token(self, token, user, expire_time):
+    def add_valid_token(self, token: str, user: str, expire_time: int):
         if not token:
             raise aiohttp.web.HTTPBadRequest(text="Must supply token")
         if not user:
@@ -48,36 +51,67 @@ class TokenCache(object):
                     break
 
 
-class KBaseAuth2(object):
+class KBaseAuth2:
     """
     A very basic KBase auth client for the Python server.
     """
 
-    def __init__(self, auth_url):
+    def __init__(self, auth_url: str):
         """
         Constructor
         """
         self._authurl = auth_url
         self._cache = TokenCache()
 
-    async def get_user(self, token):
+    async def get_user(self, token: str) -> str:
+        """
+        Takes an auth token, looks up the user info, an returns the username.
+        This gets stored in the TokenCache until the token expires, or until
+        the token's recorded cache time is up.
+        """
         if not token:
             raise aiohttp.web.HTTPBadRequest(text="Must supply token")
         user = self._cache.get_user(token)
         if user:
             return user
 
+        token_url = self._authurl + TOKEN_PATH
         async with aiohttp.ClientSession() as session:
-            async with session.get(self._authurl, headers={"Authorization": token}) as resp:
+            async with session.get(token_url, headers={"Authorization": token}) as resp:
                 ret = await resp.json()
                 if resp.reason != "OK":
                     http_code = ret["error"]["httpcode"]
                     message = ret["error"]["message"]
                     raise aiohttp.web.HTTPUnauthorized(
-                        text="Error connecting to auth service:"
+                        text="Error connecting to auth service: "
                         + f"{http_code} {resp.reason}\n{message}"
                     )
         # whichever one comes first
         self._cache._MAX_TIME_SEC = ret["cachefor"]
         self._cache.add_valid_token(token, ret["user"], ret["expires"])
         return ret["user"]
+
+    async def check_user_exists(self, username: str) -> bool:
+        """
+        Checks whether a kbase user id exists.
+        This is implemented in a roundabout way to avoid needing the user's token.
+        It uses the /login/suggestname endpoint. If the "availablename" returned
+        is different from the given name, then the id exists, and True should
+        be returned. If the "availablename" IS the given name, then that isn't
+        a user yet, and False is returned.
+        """
+        check_url = self._authurl + USERNAME_PATH + username
+        async with aiohttp.ClientSession() as session:
+            async with session.get(check_url) as resp:
+                ret = await resp.json()
+                if resp.reason != "OK":
+                    http_code = ret["error"]["httpcode"]
+                    message = ret["error"]["message"]
+                    raise aiohttp.web.HTTPError(
+                        text=f"Error connecting to auth service: {http_code} {resp.reason}\n{message}"
+                    )
+                if "availablename" not in ret:
+                    raise aiohttp.web.HTTPError(
+                        text="Malformed response from auth service: 'availablename' key not in response"
+                    )
+                return ret["availablename"] != username
